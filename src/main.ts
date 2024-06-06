1389bbf7c919665ce628733eabdd8c88db3d71d5
stq <dencher1111@mail.ru>
2024-06-07 01:05:43 +0500

    add new origin

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,23 @@ async function bootstrap() {
     origin: ['http://localhost:3001', 'https://urbancrossclient.onrender.com'],
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   const config = new DocumentBuilder()
     .setTitle('UrbanCross')
     .setDescription('api documentation')
