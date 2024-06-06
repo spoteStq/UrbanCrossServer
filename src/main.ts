15aa35e81db01ea44472e3c92712d7bd0c0424aa
stq <dencher1111@mail.ru>
2024-06-06 23:58:41 +0500

    server

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,27 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(session({
-    secret: 'keyword',
-    resave: false,
-    saveUninitialized: false,
-  }),
+  app.use(
+    session({
+      secret: 'keyword',
+      resave: false,
+      saveUninitialized: false,
+    }),
   );
   app.use(passport.initialize());
   app.use(passport.session());
 
   app.enableCors({
-    credentials:true,
+    credentials: true,
     origin: ['http://localhost:3001'],
   });
 
   const config = new DocumentBuilder()
-  .setTitle('UrbanCross')
-  .setDescription('api documentation')
-  .setVersion('1.0')
-  .addTag('api')
-  .build();
+    .setTitle('UrbanCross')
+    .setDescription('api documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
