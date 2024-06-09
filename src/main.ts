b30ebc99873045cc938a84bf32cfdd06d9a1ef60
stq <dencher1111@mail.ru>
2024-06-09 05:22:11 +0500

    new next config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import * as session from 'express-session';
+// import * as session from 'express-session';
 import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,13 +7,13 @@ import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: 'keyword',
-      resave: false,
-      saveUninitialized: false,
-    }),
-  );
+  // app.use(
+  //   session({
+  //     secret: 'keyword',
+  //     resave: false,
+  //     saveUninitialized: false,
+  //   }),
+  // );
   app.use(passport.initialize());
   // app.use(passport.session());
 
