6075e2fbbcafcc5b5390ea813daee6cec94cf6bf
stq <dencher1111@mail.ru>
2024-06-09 05:08:01 +0500

    new next config

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://urbancrossclient.onrender.com'],
+    origin: ['http://localhost:3001', 'https://urban-cross-client.vercel.app'],
   });
 
   const config = new DocumentBuilder()
