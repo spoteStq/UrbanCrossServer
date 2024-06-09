bed7494e2af706f33dec634ce7656a2bf989c82b
stq <dencher1111@mail.ru>
2024-06-09 05:20:01 +0500

    new next config

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     }),
   );
   app.use(passport.initialize());
-  app.use(passport.session());
+  // app.use(passport.session());
 
   app.enableCors({
     credentials: true,
